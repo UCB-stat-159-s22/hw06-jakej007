76ac87d7ccac5288f039616b50f2addd04b7334f
Jacob Jossart <jpjossart@berkeley.edu>
2022-04-27 23:37:48 +0000

    finally got ligotools to work

--- make_scripts/envsetup.sh
+++ make_scripts/envsetup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -i
 conda init bash
 
-mamba env create -f environment.yml -p $HOME/envs/ligo
+mamba env create -f environment.yml -p $HOME/envs/ligohw6
 
 echo ello
 echo $HOME
 conda env list
 
-conda activate ligo
+conda activate ligohw6
 
 conda env list
 
 
 pip install pytest
 
-python2 -m ipykernel install --user --name ligo 
+python2 -m ipykernel install --user --name ligohw6 
